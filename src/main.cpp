2e2b6dcd36362da24a7404de4f3b06d236b399c3
Chang Liu <48054481+HummaWhite@users.noreply.github.com>
2023-11-15 00:04:04 -0500

    ReSTIR GI strange issue : always brighter wherever there is an opening

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "Renderer.h"
 
 int main() {
-    Renderer renderer("ReSTIR PT", 1920, 1080, "res/box2.xml");
+    Renderer renderer("ReSTIR PT", 1920, 1080, "res/fireplace.xml");
     renderer.exec();
 }
